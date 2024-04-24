e2cd05010fc121dd8641ca6129f7a8d8923365a7
Sanchit Jain <jainsanchit8699@gmnail.com>
2024-04-25 04:42:19 +0530

    Working Model

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjImnR1uH1zLSetUDNKa6_KBJK1mCqeOE",
+  authDomain: "dwelling-mern.firebaseapp.com",
+  projectId: "dwelling-mern",
+  storageBucket: "dwelling-mern.appspot.com",
+  messagingSenderId: "895301359520",
+  appId: "1:895301359520:web:665d4eb196e0841246b6c5"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
